e85be60f7a155715eded317393657442c7a2831e
igor.b <igor.brevdo@gmail.com>
2019-10-26 10:50:32 +0300

    moved to mago. added proftpd.comf, inetd.conf

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # config
 Slackware linux configuration files and dotfiles
 
-The location of these files is /usr/local/config
-
 Use create_links.sh to create sym links to these config files from default locations
 like /etc/..
--- create_links.sh
+++ create_links.sh
@@ -2,31 +2,38 @@
 
 # you should re-run this script only after a new file has been added under current directory
 
+CWD=$(pwd)
+
 rm /root/.bashrc
-ln -s /usr/local/config/root/bashrc /root/.bashrc
+ln -sv ${CWD}/root/bashrc /root/.bashrc
 
 rm /root/.vimrc
-ln -s /usr/local/config/root/vimrc /root/.vimrc
+ln -sv ${CWD}/root/vimrc /root/.vimrc
 
 rm /etc/inittab
-ln -s /usr/local/config/etc/inittab /etc/inittab
+ln -sv ${CWD}/etc/inittab /etc/inittab
+
+rm /etc/inetd.conf
+ln -sv ${CWD}/etc/inetd.conf /etc/inetd.conf
+
+rm /etc/proftpd.conf
+ln -sv ${CWD}/etc/proftpd.conf /etc/proftpd.conf
 
 rm /etc/profile.d/lang.sh
-ln -s /usr/local/config/etc/profile.d/lang.sh /etc/profile.d/lang.sh
+ln -sv ${CWD}/etc/profile.d/lang.sh /etc/profile.d/lang.sh
 
 rm /etc/samba/smb.conf
-ln -s /usr/local/config/etc/samba/smb.conf /etc/samba/smb.conf
+ln -sv ${CWD}/etc/samba/smb.conf /etc/samba/smb.conf
 
 
 rm /etc/slackpkg/mirrors
-#ln -s /usr/local/config/etc/slackpkg/mirrors.current /etc/slackpkg/mirrors
-ln -s /usr/local/config/etc/slackpkg/mirrors.14.2 /etc/slackpkg/mirrors
+ln -sv ${CWD}/etc/slackpkg/mirrors.current /etc/slackpkg/mirrors
+#ln -sv ${CWD}/etc/slackpkg/mirrors.14.2 /etc/slackpkg/mirrors
 
 rm /etc/slackpkg/blacklist
-#ln -s /usr/local/config/etc/slackpkg/mirrors.current /etc/slackpkg/mirrors
-ln -s /usr/local/config/etc/slackpkg/blacklist.14.2 /etc/slackpkg/blacklist
-
+ln -sv ${CWD}/etc/slackpkg/blacklist.current /etc/slackpkg/blacklist
+#ln -sv ${CWD}/etc/slackpkg/blacklist.14.2 /etc/slackpkg/blacklist
 
 rm /etc/slackpkg/slackpkgplus.conf
-#ln -s /usr/local/config/etc/slackpkg/slackpkgplus.conf.current /etc/slackpkg/slackpkgplus.conf
-ln -s /usr/local/config/etc/slackpkg/slackpkgplus.conf.14.2 /etc/slackpkg/slackpkgplus.conf
+ln -sv ${CWD}/etc/slackpkg/slackpkgplus.conf.current /etc/slackpkg/slackpkgplus.conf
+#ln -sv ${CWD}/etc/slackpkg/slackpkgplus.conf.14.2 /etc/slackpkg/slackpkgplus.conf
